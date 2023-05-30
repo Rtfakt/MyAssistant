9b5020291bd0f383762e2e61032bf1e2e15606fb
Rtfakt <konorev8834@mail.ru>
2023-05-30 08:54:41 +0300

    убрал конфликты

--- main.py
+++ main.py
@@ -1,49 +1,17 @@
-import socket
-import time
-import Keyboards
+
 from tkinter import *
 
-#Пакеты для UDP клавиатуры
+from keyboard import keyboardTap, KeyboardPackagesUdp
+from mouse import mouseTap, MousePackagesUdp
 
-All_KEYS_KEYBOARD_UP = "345680000000000000"
-
-#ЗАДАЧА
-
-"345640000000000000"  #распиши как будут выглядеть реальные примеры
-ID = "3456"
-b1 = "4"
-b2 = "000"
-b3 = "00000"
-b4 = "00000"
-
-#Если значение положительное то должно получиться вот так
-# введя значение x = 234. находим b3.  00000 + 234 b3 должен быть равен  00234
-# введя значение y = 1234. находим b4. 00000 + 1234 должен быть равен 01234
-
-
-# дальше изи решать Не надо
-#Если x y отрицательные то
-# 65536 - 234 = 65 302
-# 65536 - 1234 = 64302
-
-#Если Null "345640000000000000"
-
-#Отправка пакета по UDP
-
-UDP_IP = "192.168.0.222"
-UDP_PORT = 12345
-MESSAGE = NONE
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
-def sendToUDP(MESSAGE) :
-    sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
-    time.sleep(0.2)
-
-
+keyboardClick = KeyboardPackagesUdp
+mouseClick = MousePackagesUdp
 
 #РЕНТГЕНОГРАФИЯ ОРГАНОВ ГРУДНОЙ КЛЕТКИ
-sendToUDP(INITIAL_MOUSE_POSITION)
+mouseTap(MousePackagesUdp.INITIAL_MOUSE_POSITION)
 #Начало
 
+
+keyboardTap(KeyboardPackagesUdp.RightArrow)
+keyboardTap(KeyboardPackagesUdp.ENTER)
 
