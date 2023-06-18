601ed1cc65f9508537c57a433d8209f0fe01fdd1
Антон <ahramcov2018@yandex.ru>
2023-06-18 21:53:04 +0300

    Уменьшил размер

--- find_text.py
+++ find_text.py
@@ -4,9 +4,7 @@ def scan_text (text):
     scan = []
     # Использование регулярных выражений для поиска:
     scan.append(re.findall(r'[0-9]{2}\.[0-9]{2}\.[0-9]{4}', text)[1])
-    scan.append(re.findall(r'[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+', text)[0].split()[0])
-    scan.append(re.findall(r'[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+', text)[0].split()[1])
-    scan.append(re.findall(r'[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+', text)[0].split()[2])
+    scan.extend(re.findall(r'[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+', text)[0].split()[0:3])
     scan.append(re.findall(r'[0-9]{2}\.[0-9]{2}\.[0-9]{4}', text)[2])
     return scan
 
